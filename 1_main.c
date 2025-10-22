bd2f43a6463d99c31dde5f0e06507a1b3aeb9173
plio <19150945470@163.com>
2025-10-22 21:38:42 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,17 @@
-//
-//
-//
-#include <stdio.h>
-int main()
+//202510303129
+//19150945470@163.com
+//陶磊
+#include<stdio.h>
+ int main()
 {
-    printf("Hello World");
-    return 0;
+   float a;
+   
+   scanf("%f,",&a);
+   float b;
+   
+   b=a*9/5+32;
+   
+   printf("%.1f",b);
+
+   return 0;
 }
