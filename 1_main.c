f5b634ccc35574aaad90509b8f2f11e236bcc92a
plio <19150945470@163.com>
2025-10-27 23:35:43 +0800

    Create 1_main.c

---
+++ 1_main.c
@@ -0,0 +1,18 @@
+#202510303129
+#2490249791@qq.com
+#陶磊
+#include<stdio.h>
+ int main()
+ {
+    int n=0;
+    scanf("%d",&n);
+    while (n>50||n%2==0||n%3==0)
+    {
+      
+     printf("密钥不安全，请重新输入");
+     scanf("%d",&n);
+    }
+    printf("密钥安全，密码设置成功");
+    
+    return 0;
+ }
